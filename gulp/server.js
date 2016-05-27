4404a28b588bfd1d9ff9a3b47b66c82ebe8768bc
Willian Ribeiro Angelo <agfoccus@gmail.com>
2016-05-27 17:05:37 -0300

    - gulp server set port 8100 for atom ionic preview

--- gulp/server.js
+++ gulp/server.js
@@ -11,13 +11,11 @@ var util = require('util');
 
 var proxyMiddleware = require('http-proxy-middleware');
 
-function browserSyncInit(baseDir, browser)
-{
+function browserSyncInit(baseDir, browser) {
     browser = browser === undefined ? 'default' : browser;
 
     var routes = null;
-    if ( baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1) )
-    {
+    if (baseDir === conf.paths.src || (util.isArray(baseDir) && baseDir.indexOf(conf.paths.src) !== -1)) {
         routes = {
             '/bower_components': 'bower_components'
         };
@@ -25,7 +23,7 @@ function browserSyncInit(baseDir, browser)
 
     var server = {
         baseDir: baseDir,
-        routes : routes
+        routes: routes
     };
 
     /*
@@ -38,32 +36,29 @@ function browserSyncInit(baseDir, browser)
     // server.middleware = proxyMiddleware('/users', {target: 'http://jsonplaceholder.typicode.com', changeOrigin: true});
 
     browserSync.instance = browserSync.init({
+        port: 8100,
         startPath: '/',
-        server   : server,
-        browser  : browser
+        server: server,
+        browser: browser
     });
 }
 
 browserSync.use(browserSyncSpa({
-    selector: '[ng-app]'// Only needed for angular apps
+    selector: '[ng-app]' // Only needed for angular apps
 }));
 
-gulp.task('serve', ['watch'], function ()
-{
+gulp.task('serve', ['watch'], function () {
     browserSyncInit([path.join(conf.paths.tmp, '/serve'), conf.paths.src]);
 });
 
-gulp.task('serve:dist', ['build'], function ()
-{
+gulp.task('serve:dist', ['build'], function () {
     browserSyncInit(conf.paths.dist);
 });
 
-gulp.task('serve:e2e', ['inject'], function ()
-{
+gulp.task('serve:e2e', ['inject'], function () {
     browserSyncInit([conf.paths.tmp + '/serve', conf.paths.src], []);
 });
 
-gulp.task('serve:e2e-dist', ['build'], function ()
-{
+gulp.task('serve:e2e-dist', ['build'], function () {
     browserSyncInit(conf.paths.dist, []);
 });
