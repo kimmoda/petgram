b53935e3bdb39d4945a3a7015a5454e344c90ae5
Willian Ribeiro Angelo <agfoccus@gmail.com>
2016-05-28 01:19:23 -0300

    - Language config migrate to ion-language directive

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -8,7 +8,6 @@
         .run(runIonic)
         .run(runFacebook)
         .config(configCompile)
-        .config(configLanguage)
         .config(configFacebook)
         .config(configIonic);
 
@@ -49,35 +48,6 @@
         });
 
 
-    }
-
-    function configLanguage($translateProvider, AppConfig, tmhDynamicLocaleProvider) {
-
-        // angular-translate configuration
-        $translateProvider.useLoader('$translatePartialLoader', {
-            urlTemplate: '{part}/i18n/{lang}.json'
-        });
-        $translateProvider.useSanitizeValueStrategy(null);
-
-        // Translate Config
-        $translateProvider.useMissingTranslationHandlerLog();
-        $translateProvider.useLocalStorage(); // saves selected language to localStorage
-        tmhDynamicLocaleProvider.localeLocationPattern('../bower_components/angular-i18n/angular-locale_{{locale}}.js');
-
-        var langvar     = navigator.language || navigator.userlanguage;
-        var userlangvar = langvar.split('-')[0];
-        var language    = AppConfig.preferredLocale;
-        var searchLang  = _.some(AppConfig.locales, {
-            code: userlangvar
-        });
-        if (searchLang) {
-            language = _.filter(AppConfig.locales,{code: searchLang})[0];
-        }
-        console.log(language);
-        $translateProvider.preferredLanguage(language);
-        moment.locale(language);
-
-        console.log(searchLang, language);
     }
 
     function configIonic($ionicConfigProvider) {
