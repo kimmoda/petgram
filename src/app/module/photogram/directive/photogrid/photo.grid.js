b434a426e07d958a4a52583a90da182aefd602f6
Willian Ribeiro Angelo <agfoccus@gmail.com>
2016-06-01 10:20:13 -0300

    - Photogrid open first cache feed

--- src/app/module/photogram/directive/photogrid/photo.grid.js
+++ src/app/module/photogram/directive/photogrid/photo.grid.js
@@ -1,179 +1,177 @@
 (function () {
-  'use strict';
-  angular
-      .module('app.photogram')
-      .directive('photogramPhotoGrid', photogramPhotoGrid);
+    'use strict';
+    angular
+        .module('app.photogram')
+        .directive('photogramPhotoGrid', photogramPhotoGrid);
 
-  function photogramPhotoGrid(AppConfig, Photogram, $ionicPopup, PhotogramShare, PhotogramFeedback, $ionicActionSheet) {
-    var path = AppConfig.path;
+    function photogramPhotoGrid(AppConfig, Photogram, $ionicPopup, PhotogramShare, PhotogramFeedback, $ionicActionSheet) {
+        var path = AppConfig.path;
 
-    return {
-      restrict: 'E',
-      scope: {
-        user: '=',
-        profile: '='
-      },
-      templateUrl: path + '/directive/photogrid/photo-grid.html',
-      link: photogramPhotoGridCtrl
-    };
-
-    function photogramPhotoGridCtrl($scope, elem, attr) {
-
-      $scope.action  = action;
-      $scope.like    = likePhoto;
-      $scope.loading = true;
-      $scope.load    = feed;
-      $scope.load();
-      $scope.loadMore = loadMore;
-      $scope.gallery = {
-        src: ''
-      };
-
-      init();
-
-      $scope.$on('PhotogramHome:reload', function () {
-        loadMore(true);
-      });
-
-      function init() {
-        $scope.data  = {
-          total: -1,
-          galleries: []
+        return {
+            restrict: 'E',
+            scope: {
+                user: '=',
+                profile: '='
+            },
+            templateUrl: path + '/directive/photogrid/photo-grid.html',
+            link: photogramPhotoGridCtrl
         };
-        $scope.page  = 0;
-        $scope.empty = false;
-        $scope.more  = false;
-      }
 
+        function photogramPhotoGridCtrl($scope, elem, attr) {
 
-      function loadMore(force) {
-        console.log('Load More', $scope.more);
-        $scope.load(force);
-      }
+            $scope.action  = action;
+            $scope.like    = likePhoto;
+            $scope.loading = true;
+            $scope.feed    = feed;
+            $scope.gallery = {
+                src: ''
+            };
 
+            init();
 
-      function feed(force) {
-        // console.log('Load ');
+            $scope.$on('PhotogramHome:reload', function () {
+                feed(true);
+            });
 
-        if (force) {
-          init();
-        }
-
-        Photogram
-            .feedGrid($scope.page, $scope.user)
-            .then(function (resp) {
-
-              console.log(resp);
-              resp.rows.map(function (item) {
-                item.progress     = false;
-                $scope.data.total = resp.total;
-                $scope.data.galleries.push(item);
-              });
-
-              // console.log('qtd', resp.length);
-
-              if (resp.length) {
-                $scope.more = true;
-                $scope.page++;
-              } else {
-                $scope.empty = true;
+            function init() {
+                $scope.data  = {
+                    total: -1,
+                    galleries: []
+                };
+                $scope.page  = 0;
+                $scope.empty = false;
                 $scope.more  = false;
-              }
-            })
-            .then(function () {
-              $scope.loading = false;
-              $scope.$broadcast('scroll.refreshComplete');
-              $scope.$broadcast('scroll.infiniteScrollComplete');
-
-            })
-            .catch(function () {
-              $scope.$broadcast('scroll.refreshComplete');
-              $scope.$broadcast('scroll.infiniteScrollComplete');
-              if ($scope.data.length) {
-                $scope.loading = false;
-                $scope.page++;
-              } else {
-                $scope.empty   = true;
-                $scope.loading = false;
-              }
-              $scope.more = false;
-            });
-      }
-
-      function likePhoto(gallery) {
-        gallery.item.liked = !gallery.item.liked;
-        Photogram
-            .likeGallery(gallery.id)
-            .then(function (resp) {
-              gallery.item.qtdLike = resp.likes;
-            });
-      }
-
-      function action(gallery) {
-
-        var buttons = [{
-          text: '<i class="icon ion-share"></i>' + ('Share')
-        }, {
-          text: '<i class="icon ion-alert-circled"></i>' + ('Report')
-        }];
-
-        var user = Parse.User.current();
-
-        if (user.id === gallery.user.id) {
-          var buttonDelete = {
-            text: '<i class="icon ion-trash-b"></i>' + ('Delete your photo')
-          };
-          buttons.push(buttonDelete);
-        }
-        var message     = {
-          image: gallery.src,
-          text: gallery.item.title
-        }
-        var actionSheet = {
-          buttons: buttons,
-          titleText: ('Photo'),
-          cancelText: ('Cancel'),
-          buttonClicked: actionButtons
-        };
+                feed(false,true);
+            }
 
 
-        function actionButtons(index) {
-          switch (index) {
-            case 0:
-              PhotogramShare.share(message);
-              break;
-            case 1:
-              PhotogramFeedback.modal(gallery);
-              break;
-            case 2:
+            function feed(force, cache) {
+                // console.log('Load ');
 
-              $ionicPopup
-                  .confirm({
-                    title: ('Delete photo'),
-                    template: ('Are you sure?')
-                  })
-                  .then(function (res) {
-                    if (res) {
-                      Photogram
-                          .deletePhoto(gallery.id)
-                          .then(function () {
-                            console.log('Photo deleted');
-                            $scope.$emit('PhotogramHome:reload');
-                          });
+                if (force) {
+                    init();
+                }
+
+                var service = Photogram.getOfflineFeed($scope.page, $scope.user);
+
+                if (!cache) {
+                    service = Photogram.feedGrid($scope.page, $scope.user)
+                }
+
+                service
+                    .then(function (resp) {
+
+                        console.log(resp);
+                        resp.rows.map(function (item) {
+                            item.progress     = false;
+                            $scope.data.total = resp.total;
+                            $scope.data.galleries.push(item);
+                        });
+
+                        // console.log('qtd', resp.length);
+
+                        if (resp.length) {
+                            $scope.more = true;
+                            $scope.page++;
+                        } else {
+                            $scope.empty = true;
+                            $scope.more  = false;
+                        }
+                    })
+                    .then(function () {
+                        $scope.loading = false;
+                        $scope.$broadcast('scroll.refreshComplete');
+                        $scope.$broadcast('scroll.infiniteScrollComplete');
+
+                    })
+                    .catch(function () {
+                        $scope.$broadcast('scroll.refreshComplete');
+                        $scope.$broadcast('scroll.infiniteScrollComplete');
+                        if ($scope.data.length) {
+                            $scope.loading = false;
+                            $scope.page++;
+                        } else {
+                            $scope.empty   = true;
+                            $scope.loading = false;
+                        }
+                        $scope.more = false;
+                    });
+            }
+
+            function likePhoto(gallery) {
+                gallery.item.liked = !gallery.item.liked;
+                Photogram
+                    .likeGallery(gallery.id)
+                    .then(function (resp) {
+                        gallery.item.qtdLike = resp.likes;
+                    });
+            }
+
+            function action(gallery) {
+
+                var buttons = [{
+                    text: '<i class="icon ion-share"></i>' + ('Share')
+                }, {
+                    text: '<i class="icon ion-alert-circled"></i>' + ('Report')
+                }];
+
+                var user = Parse.User.current();
+
+                if (user.id === gallery.user.id) {
+                    var buttonDelete = {
+                        text: '<i class="icon ion-trash-b"></i>' + ('Delete your photo')
+                    };
+                    buttons.push(buttonDelete);
+                }
+                var message     = {
+                    image: gallery.src,
+                    text: gallery.item.title
+                }
+                var actionSheet = {
+                    buttons: buttons,
+                    titleText: ('Photo'),
+                    cancelText: ('Cancel'),
+                    buttonClicked: actionButtons
+                };
+
+
+                function actionButtons(index) {
+                    switch (index) {
+                        case 0:
+                            PhotogramShare.share(message);
+                            break;
+                        case 1:
+                            PhotogramFeedback.modal(gallery);
+                            break;
+                        case 2:
+
+                            $ionicPopup
+                                .confirm({
+                                    title: ('Delete photo'),
+                                    template: ('Are you sure?')
+                                })
+                                .then(function (res) {
+                                    if (res) {
+                                        Photogram
+                                            .deletePhoto(gallery.id)
+                                            .then(function () {
+                                                console.log('Photo deleted');
+                                                $scope.$emit('PhotogramHome:reload');
+                                            });
+                                    }
+                                });
+
+
                     }
-                  });
+                    return true;
+                }
 
-
-          }
-          return true;
+                // Show the action sheet
+                $ionicActionSheet.show(actionSheet);
+            }
         }
-        
-        // Show the action sheet
-        $ionicActionSheet.show(actionSheet);
-      }
-    }
 
-  }
+    }
 
 
 })();
